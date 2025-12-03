459ee78a8188461682b51da1e3ca914599c3a88d
Lex Rex <lexnaturale@pm.me>
2025-12-03 17:05:24 +0200

    Update build

--- build/csgfd58n.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,o,r as a}from"./BFFrjTPi.js";const c={},n={class:"main"};function r(e,_){return o(),t("main",n,[a(e.$slots,"default")])}const f=s(c,[["render",r]]);export{f as default};
